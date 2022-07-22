993a6645ffa243fc8ffdd44575b83a3d606defe0
Hugo Gayoso Meira Suassuna de Medeiros <hugo.medeiros@virtus.ufcg.edu.br>
2022-07-22 08:58:26 -0300

    Included Dedicaded functions

--- Python_files/test.py
+++ Python_files/test.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def show_img_grey(grey_img: np.ndarray):
+    plt.imshow(demo_img,cmap='gray',vmin=0,vmax=255)
+    plt.show()
+
 
 demo_img = np.zeros((9,9))
 
-demo_img[0:5,:] = 255
-demo_img[0:5,:] = 120
+demo_img[:5,:] = 200
+demo_img[5:,:] = 120
 
+show_img_grey(demo_img)
 
-plt.imshow(demo_img,cmap='gray')
-plt.show()
