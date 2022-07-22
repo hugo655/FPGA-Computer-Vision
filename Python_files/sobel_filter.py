3dc0f7429b30104dddd5b11822eae45d16414601
hugo655 <gayosohugo@gmail.com>
2022-07-22 07:08:54 -0800

    Added grey scale conversion

--- Python_files/sobel_filter.py
+++ Python_files/sobel_filter.py
@@ -1,14 +1,32 @@
 #!/bin/python
 
+from re import I
 import sys
 import os
 import imageio as iio
 import matplotlib.pyplot as plt
+import numpy as np
+
+def rgb2grey(rgb_img: np.ndarray):
+    grey_img = 0.299*rgb_img[:,:,0] + 0.587*rgb_img[:,:,1] + 0.114*rgb_img[:,:,2]    
+    return grey_img
 
 # Import image
 image_path = sys.argv[1]
-image = iio.imread(image_path)
+image = np.array(iio.imread(image_path))
 
-print(image.shape)
-plt.imshow(image)
+#Convert to Grey Scale
+grey_img = rgb2grey(image)
+
+y_dim, x_dim = grey_img.shape
+
+print(x_dim, y_dim)
+
+#Print image in grey scale
+plt.imshow(grey_img,cmap='gray')
 plt.show()
+
+
+# print(image.shape)
+# plt.imshow(image)
+# plt.show()
