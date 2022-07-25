f3fb3541632866a57ae3f7f34ca0d01eaafd1ea3
Hugo Gayoso Meira Suassuna de Medeiros <hugo.medeiros@virtus.ufcg.edu.br>
2022-07-25 15:19:31 -0300

    Added tests to validate cov and correlation

--- Python_files/test_image_toolbox.py
+++ Python_files/test_image_toolbox.py
@@ -10,12 +10,32 @@ class Test_image_toolbox(unittest.TestCase):
         np.testing.assert_array_equal(my_array, ref_array)
 
     def test_corr(self):
-        demo_img = it.impulse_5x5()
+        demo_img = it.gradient_generator(2,100)
         w = it.kernel_generator(0)
         output_corr = it.corr(demo_img,w)
         output_cv = cv.filter2D(src=demo_img, ddepth=-1, kernel=w)
-        np.testing.assert_array_equal(output_cv, output_corr)
 
+        #Remove the border
+        output_corr = np.delete(output_corr,[0,-1],0)
+        output_corr = np.delete(output_corr,[0,-1],1)
+        output_cv = np.delete(output_cv,[0,-1],0)
+        output_cv = np.delete(output_cv,[0,-1],1)
+
+        np.testing.assert_array_equal(output_cv, output_corr)
+   
+    def test_cov(self):
+        demo_img = it.gradient_generator(2,100)
+        w = it.kernel_generator(1)
+        output_cov = it.cov(demo_img,w)
+        output_cv = cv.filter2D(src=demo_img, ddepth=-1, kernel=w[::-1,::-1])
+
+        #Remove the border
+        output_cov = np.delete(output_cov,[0,-1],0)
+        output_cov = np.delete(output_cov,[0,-1],1)
+        output_cv = np.delete(output_cv,[0,-1],0)
+        output_cv = np.delete(output_cv,[0,-1],1)
+
+        np.testing.assert_array_equal(output_cv, output_cov)
 
 if __name__=='__main__':
 	unittest.main()
